cfc8a39a21bbf721f12897dab7c1064517af5482
8Eliauk <h871587105@163.com>
2024-09-15 22:24:24 +0800

    初始文件

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup name="App">
+import MyHeader from './components/MyHeader.vue';
+import MyList from './components/MyList.vue';
+import MyFooter from './components/MyFooter.vue';
+</script>
+
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader/>
+      <MyList/>
+      <MyFooter/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
